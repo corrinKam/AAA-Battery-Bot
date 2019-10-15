18d3c1790087e510b5c32532da014c077721f5e6
Veronica Lee <majorprocrastinator58@gmail.com>
2019-10-15 19:19:47 +1100

    hug i think

--- bot.js
+++ bot.js
@@ -131,8 +131,7 @@ bot.on('message', message => {
                 let responseFinal = response.data[responseIndex];
                 //gives single result
 
-                message.channel.send(member.id + " " + "\n" + responseFinal.images.fixed_height.url);
-                //message.channel.send("@" + member.id);
+                message.channel.send(responseFinal.images.fixed_height.url);
             }).catch(() => {
                 message.channel.send('> !ERROR!');
             })
